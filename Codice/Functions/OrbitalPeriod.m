06595c43b34c8393cf1154c219d99b3698d2c31b
Capamazz <luca.mazzotti.22@gmail.com>
2022-10-03 14:33:11 +0200

    Update OrbitalPeriod.m

--- Codice/Functions/OrbitalPeriod.m
+++ Codice/Functions/OrbitalPeriod.m
@@ -1,8 +1,11 @@
-function [T] = OrbitalPeriod(r,M)
+function [T] = orbitalPeriod(a,M)
 % The function allows to calculate the orbital period T from the radius.
-% Input:
-% r - radius of orbit
+% INPUT:
+% a - ellipse major apsis
 % M - mass of the planet (If left unspecified erth mass is used)
+%
+% OUTPUT:
+% T - orbital period
 
 % To call this function type[ P ] = orbital period ( R )
 
@@ -14,6 +17,6 @@ if nargin == 1
     M = 5.98e24;
 end
 
-T = sqrt((4*(pi^2)*r^3)/(G*M));
+T = sqrt((4*(pi^2)*a^3)/(G*M));
 
 end
